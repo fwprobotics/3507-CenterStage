989fdef20624fc246da20f5aec69657d41288ee0
js802025 <30957566+js802025@users.noreply.github.com>
2023-09-17 22:34:03 -0500

    Update WallProcessor.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/WallProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pipelines/WallProcessor.java
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.pipelines;
-
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.navigation.*;
+import org.firstinspires.ftc.teamcode.pipelines.WallProcessor.COLORS;
 import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -18,8 +18,11 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.apriltag.AprilTagDetectorJNI;
 import org.openftc.easyopencv.OpenCvPipeline;
-
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Size;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class WallProcessor extends OpenCvPipeline
 {
@@ -29,6 +32,29 @@ public class WallProcessor extends OpenCvPipeline
     public static Scalar red = new Scalar(255,0,0,255);
     public static Scalar green = new Scalar(0,255,0,255);
     public static Scalar white = new Scalar(255,255,255,255);
+
+    public static Scalar green_lower = new Scalar(59.5, 38.3, 14.2);
+    public static Scalar green_upper = new Scalar(195.5, 109.1, 167.2);
+    public static Scalar white_lower = new Scalar(129, 113, 0);
+    public static Scalar white_upper = new Scalar(255, 255, 255);
+    public static Scalar yellow_lower = new Scalar(69.4, 0, 175.7);
+    public static Scalar yellow_upper = new Scalar(255, 168.6, 255.0);
+    public static Scalar purple_lower = new Scalar(0, 0, 0);
+    public static Scalar purple_upper = new Scalar(255, 255, 120.4);
+
+    public enum COLORS {
+        PURPLE (purple_lower, purple_upper),
+        YELLOW (yellow_lower, yellow_upper),
+        WHITE (white_lower, white_upper);
+
+        public Scalar lower;
+        public Scalar upper;
+
+        COLORS(Scalar lower, Scalar upper) {
+            this.lower = lower;
+            this.upper = upper;
+        }
+    }
 
     static final double FEET_PER_METER = 3.28084;
 
@@ -62,6 +88,7 @@ public class WallProcessor extends OpenCvPipeline
     private boolean needToSetDecimation;
     private final Object decimationSync = new Object();
 
+
     Telemetry telemetry;
 
     public WallProcessor(Telemetry telemetry) {
@@ -87,69 +114,145 @@ public class WallProcessor extends OpenCvPipeline
     public Mat processFrame(Mat input)
     {
         // Convert to greyscale
+
+        //  input = getPixelsByColor(input, COLORS.WHITE);
+        List<MatOfPoint> contours = getPixelsByColor(input, COLORS.WHITE);
+
+        input = labelPixels(input, contours);
+        //  labelPixels(input, purple_contours, COLORS.PURPLE);
+        synchronized (decimationSync)
+        {
+            if(needToSetDecimation)
+            {
+                AprilTagDetectorJNI.setApriltagDetectorDecimation(nativeApriltagPtr, decimation);
+                needToSetDecimation = false;
+            }
+        }
+
+        // Run AprilTag
+        detections = AprilTagDetectorJNI.runAprilTagDetectorSimple(nativeApriltagPtr, grey, TAG_SIZE, fx, fy, cx, cy);
+
+        synchronized (detectionsUpdateSync)
+        {
+            detectionsUpdate = detections;
+        }
+
+        // For fun, use OpenCV to draw 6DOF markers on the image. We actually recompute the pose using
+        // OpenCV because I haven't yet figured out how to re-use AprilTag's pose in OpenCV.
+        if (detections.size() > 0) {
+            AprilTagDetection d = detections.get(0);
+            Imgproc.circle(input, d.corners[3], 5, new Scalar(255, 0, 0, 255), -1);
+            Imgproc.circle(input, d.corners[1], 5, new Scalar(0, 255, 0, 255), -1);
+            telemetry.addLine("corner 3: " + d.corners[3].toString());
+            telemetry.addLine("corner 1: " + d.corners[1].toString());
+
+            Rect crop = new Rect((int) d.corners[3].x, (int)d.corners[3].y, (int) ((int) d.corners[1].x-d.corners[3].x), (int) ((int)d.corners[1].y-d.corners[3].y));
+            input = new Mat(input, crop);
+        }
+        for(AprilTagDetection detection : detections)
+        {
+
+//            Pose pose = poseFromTrapezoid(detection.corners, cameraMatrix, tagsizeX, tagsizeY);
+//            drawAxisMarker(input, tagsizeY/2.0, 6, pose.rvec, pose.tvec, cameraMatrix);
+//            draw3dCubeMarker(input, tagsizeX, tagsizeX, tagsizeY, 5, pose.rvec, pose.tvec, cameraMatrix);
+//
+            Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+
+            telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+            telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+            telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+            telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+
+            telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
+            telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
+            telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
+        }
+
+        telemetry.update();
+
+        return input;
+    }
+    public List<MatOfPoint> getPixelsByColor(Mat input, COLORS color) {
+
+        Imgproc.cvtColor(input, grey, Imgproc.COLOR_RGB2GRAY);
+
+
         Mat mat = new Mat();
         Imgproc.cvtColor(input, mat, Imgproc.COLOR_RGB2Lab);
-        Rect crop = new Rect(0, input.height()/2, input.width(), input.height()/2);
-        mat = new Mat(mat, crop);
-        Scalar lower = new Scalar(126, 71, 108);
-        Scalar upper = new Scalar(166, 111, 148);
+        //  Rect crop = new Rect(0, input.height()/2, input.width(), input.height()/2);
+        //  mat = new Mat(mat, crop);
+
 
         Mat thresh = new Mat();
 
-        Core.inRange(mat, lower, upper, thresh);
+        Core.inRange(mat, color.lower, color.upper, thresh);
+        List<MatOfPoint> contours = new ArrayList();
+
+        Mat heirarchy = new Mat();
+        Imgproc.findContours(thresh, contours, heirarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
 
 
-//         synchronized (decimationSync)
-//         {
-//             if(needToSetDecimation)
-//             {
-//                 AprilTagDetectorJNI.setApriltagDetectorDecimation(nativeApriltagPtr, decimation);
-//                 needToSetDecimation = false;
-//             }
-//         }
 
-//         // Run AprilTag
-//         detections = AprilTagDetectorJNI.runAprilTagDetectorSimple(nativeApriltagPtr, grey, TAG_SIZE, fx, fy, cx, cy);
 
-//         synchronized (detectionsUpdateSync)
-//         {
-//             detectionsUpdate = detections;
-//         }
+        return contours;
+    }
 
-//         // For fun, use OpenCV to draw 6DOF markers on the image. We actually recompute the pose using
-//         // OpenCV because I haven't yet figured out how to re-use AprilTag's pose in OpenCV.
-//         if (detections.size() > 0) {
-//         AprilTagDetection d = detections.get(0);
-//         Imgproc.circle(input, d.corners[3], 5, new Scalar(255, 0, 0, 255), -1);
-//         Imgproc.circle(input, d.corners[1], 5, new Scalar(0, 255, 0, 255), -1);
-//         telemetry.addLine("corner 3: " + d.corners[3].toString());
-//         telemetry.addLine("corner 1: " + d.corners[1].toString());
+    public Mat labelPixels(Mat input, List<MatOfPoint> contours) {
+        for (MatOfPoint contour : contours) {
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+            MatOfPoint2f contour2f = new MatOfPoint2f(contour.toArray());
+            double epsilon = 0.04 * Imgproc.arcLength(contour2f, true);
+            Imgproc.approxPolyDP(contour2f, approxCurve, epsilon, true);
 
-//         Rect crop = new Rect((int) d.corners[3].x, (int)d.corners[3].y, (int) ((int) d.corners[1].x-d.corners[3].x), (int) ((int)d.corners[1].y-d.corners[3].y));
-//         input = new Mat(input, crop);
-//         }
-//         for(AprilTagDetection detection : detections)
-//         {
+            Point[] points = approxCurve.toArray();
+            int numVertices = points.length;
 
-// //            Pose pose = poseFromTrapezoid(detection.corners, cameraMatrix, tagsizeX, tagsizeY);
-// //            drawAxisMarker(input, tagsizeY/2.0, 6, pose.rvec, pose.tvec, cameraMatrix);
-// //            draw3dCubeMarker(input, tagsizeX, tagsizeX, tagsizeY, 5, pose.rvec, pose.tvec, cameraMatrix);
-// //
-//             Orientation rot = Orientation.getOrientation(detection.pose.R, AxesReference.INTRINSIC, AxesOrder.YXZ, AngleUnit.DEGREES);
+            Rect rect = Imgproc.boundingRect(contour);
 
-//             telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
-//             telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
-//             telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
-//             telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
 
-//             telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", rot.firstAngle));
-//             telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", rot.secondAngle));
-//             telemetry.addLine(String.format("Rotation Roll: %.2f degrees", rot.thirdAngle));
-//         }
+            // Convert the MatOfPoint2f back to MatOfPoint
+            MatOfPoint approxContour = new MatOfPoint();
+            approxCurve.convertTo(approxContour, CvType.CV_32S);
 
-//         telemetry.update();
-
+            if (numVertices == 6 && (rect.width < 25 && rect.width > 10)) {
+                COLORS color = getPixelColor(input, approxContour);
+                //  Draw the approximate polygon
+                List<MatOfPoint> drawContours = new ArrayList<>();
+                drawContours.add(approxContour);
+                Imgproc.drawContours(input, drawContours, 0, new Scalar(0, 255, 0), 2);
+                Point point = new Point(rect.x + rect.width / 2, rect.y + rect.height / 2);
+                Imgproc.circle(input, point, 5, new Scalar(255, 0, 0, 255), -1);
+                Imgproc.putText(input, color.toString(), new Point(points[0].x, points[0].y), Imgproc.FONT_HERSHEY_COMPLEX, 0.25, new Scalar(0, 0, 255));
+            }
+        }
         return input;
+    }
+
+    public COLORS getPixelColor(Mat input, MatOfPoint contour) {
+
+        Mat contourMat = new Mat(input, Imgproc.boundingRect(contour));
+        Mat mat = new Mat();
+        Imgproc.cvtColor(contourMat, mat, Imgproc.COLOR_RGB2Lab);
+        //  Rect crop = new Rect(0, input.height()/2, input.width(), input.height()/2);
+        //  mat = new Mat(mat, crop);
+        for (COLORS color : COLORS.values()) {
+            Mat thresh = new Mat();
+
+            Core.inRange(mat, color.lower, color.upper, thresh);
+            List<MatOfPoint> contours = new ArrayList();
+            Mat heirarchy = new Mat();
+            Imgproc.findContours(thresh, contours, heirarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+            if (contours.size() >0) {
+                return color;
+            }
+        }
+
+
+
+        return COLORS.WHITE;
+
     }
 
     public void setDecimation(float decimation)
